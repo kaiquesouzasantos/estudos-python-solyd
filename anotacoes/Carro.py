d845f92df9874e02f9a3703a5ba2583e425a7575
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-02-20 13:29:36 -0300

    Delete anotacoes directory

--- anotacoes/Carro.py
+++
@@ -1,6 +0,0 @@
-# ===================== Orientação a Objetos =====================
-from Veiculo import Veiculo
-
-class Carro(Veiculo): # (Veiculo) => herda a classe
-    def __init__(self,cor,rodas,marca,tanque):
-        Veiculo.__init__(self,cor,rodas,marca,tanque)
